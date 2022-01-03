49fa5ef449488e975f568f8ef8e23189bb999c95
Umit Kablan <umitkablan@gmail.com>
2022-01-03 12:49:48 +0300

    Decide function to decide about response packets in test

--- core_test.cpp
+++ core_test.cpp
@@ -27,6 +27,8 @@ public:
     DoViewCh,
     StartView,
     Prepare,
+    StartViewResponse,
+    PrepareResponse,
   };
 
   FakeTMsgBuggyNetwork(std::function<int(int, int, TstMsgType, int)> decFun, bool shuffle = false)
@@ -118,8 +120,12 @@ public:
       } else
         return ret;
 
-      std::lock_guard<std::mutex> lck(engines_mtxs_[from]);
-      return engines_[from]->ConsumeReply(to, svr);
+      ret = callDecideSync(from, to, TstMsgType::StartViewResponse, sv.view);
+      if (!ret) {
+        std::lock_guard<std::mutex> lck(engines_mtxs_[from]);
+        return engines_[from]->ConsumeReply(to, svr);
+      }
+      return ret;
     });
   }
 
@@ -133,17 +139,13 @@ public:
         presp = engines_[to]->ConsumeMsg(from, pr);
       } else return ret;
 
-      std::lock_guard<std::mutex> lck(engines_mtxs_[from]);
-      return engines_[from]->ConsumeReply(to, presp);
+      ret = callDecideSync(from, to, TstMsgType::PrepareResponse, pr.view);
+      if (!ret) {
+        std::lock_guard<std::mutex> lck(engines_mtxs_[from]);
+        return engines_[from]->ConsumeReply(to, presp);
+      }
+      return ret;
     });
-  }
-
-  void SendMsg(int from, int to, const MsgStartViewResponse& svr) override
-  {
-  }
-
-  void SendMsg(int from, int to, const MsgPrepareResponse& pr) override
-  {
   }
 
 private:

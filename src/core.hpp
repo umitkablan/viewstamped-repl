5b7ffa25e5dc6781bd57b1c2ba6e308acde4ce46
umitkablan <umitkablan@gmail.com>
2022-01-27 13:19:35 +0300

    minor: consistent tab width -> 2 spaces (#20)

--- src/core.hpp
+++ src/core.hpp
@@ -17,82 +17,82 @@ enum class Status : char {
 template <typename TMsgDispatcher, typename TStateMachine>
 class ViewstampedReplicationEngine {
 public:
-    ViewstampedReplicationEngine(int totreplicas, int replica, TMsgDispatcher& dp, TStateMachine& sm,
-        std::chrono::milliseconds tick_interval = std::chrono::milliseconds(150));
+  ViewstampedReplicationEngine(int totreplicas, int replica, TMsgDispatcher& dp, TStateMachine& sm,
+    std::chrono::milliseconds tick_interval = std::chrono::milliseconds(150));
 
-    void Start();
-    void Stop();
+  void Start();
+  void Stop();
 
-    std::variant<std::monostate, MsgLeaderRedirect, int>
-        ConsumeMsg(const MsgClientOp&);
-    int ConsumeMsg(int from, const MsgStartViewChange&);
-    int ConsumeMsg(int from, const MsgDoViewChange&);
-    MsgStartViewResponse ConsumeMsg(int from, const MsgStartView&);
-    MsgPrepareResponse ConsumeMsg(int from, const MsgPrepare&);
-    MsgMissingLogsResponse
-        ConsumeMsg(int from, const MsgGetMissingLogs&);
-    MsgOpPersistedResponse
-        ConsumeMsg(int from, const MsgOpPersistedQuery&);
+  std::variant<std::monostate, MsgLeaderRedirect, int>
+    ConsumeMsg(const MsgClientOp&);
+  int ConsumeMsg(int from, const MsgStartViewChange&);
+  int ConsumeMsg(int from, const MsgDoViewChange&);
+  MsgStartViewResponse ConsumeMsg(int from, const MsgStartView&);
+  MsgPrepareResponse ConsumeMsg(int from, const MsgPrepare&);
+  MsgMissingLogsResponse
+    ConsumeMsg(int from, const MsgGetMissingLogs&);
+  MsgOpPersistedResponse
+    ConsumeMsg(int from, const MsgOpPersistedQuery&);
 
-    int ConsumeReply(int from, const MsgPrepareResponse&);
-    int ConsumeReply(int from, const MsgStartViewResponse&);
-    int ConsumeReply(int from, const MsgMissingLogsResponse&);
+  int ConsumeReply(int from, const MsgPrepareResponse&);
+  int ConsumeReply(int from, const MsgStartViewResponse&);
+  int ConsumeReply(int from, const MsgMissingLogsResponse&);
 
-    int View() const { return view_; }
-    Status GetStatus() const { return status_; }
-    int CommitID() const { return commit_; }
-    int OpID() const { return op_; }
-    const std::vector<std::pair<int, MsgClientOp>>&
-        GetCommittedLogs() const { return logs_; }
-    std::size_t GetHash() const noexcept { return log_hash_; }
+  int View() const { return view_; }
+  Status GetStatus() const { return status_; }
+  int CommitID() const { return commit_; }
+  int OpID() const { return op_; }
+  const std::vector<std::pair<int, MsgClientOp>>&
+    GetCommittedLogs() const { return logs_; }
+  std::size_t GetHash() const noexcept { return log_hash_; }
 
-    void HealthTimeoutTicked();
-
-private:
-    TMsgDispatcher& dispatcher_;
-    TStateMachine& state_machine_;
-    std::chrono::milliseconds tick_interval_;
-
-    const int totreplicas_;
-    const int replica_;
-    int view_;
-    Status status_;
-    int op_;
-    int commit_;
-    std::size_t log_hash_;
-    std::vector<std::pair<int, MsgClientOp>> logs_;
-    MsgClientOp cliop_;
-
-    bool prepare_sent_;
-    unsigned latest_healthtick_received_;
-    unsigned healthcheck_tick_;
+  void HealthTimeoutTicked();
 
 private:
-    struct trackDups {
-      trackDups(int totreplicas, int emptyID = -1)
-        : recv_replicas_(totreplicas * totreplicas, 0)
-        , recv_views_(totreplicas, emptyID)
-        , empty_id (emptyID)
-        {}
-      std::vector<int8_t> recv_replicas_;
-      std::vector<int> recv_views_;
-      int empty_id;
-    };
-    trackDups trackDups_SVCs_;
-    trackDups trackDups_DVCs_;
-    trackDups trackDups_PrepResps_;
-    trackDups trackDups_SVResps_;
-    std::vector<MsgStartViewResponse> svResps_;
+  TMsgDispatcher& dispatcher_;
+  TStateMachine& state_machine_;
+  std::chrono::milliseconds tick_interval_;
 
-    std::pair<bool,int>
-    checkDuplicate(trackDups&, int from, int view);
-    void clearDupsEntry(trackDups& td, int idx = -1);
+  const int totreplicas_;
+  const int replica_;
+  int view_;
+  Status status_;
+  int op_;
+  int commit_;
+  std::size_t log_hash_;
+  std::vector<std::pair<int, MsgClientOp>> logs_;
+  MsgClientOp cliop_;
+
+  bool prepare_sent_;
+  unsigned latest_healthtick_received_;
+  unsigned healthcheck_tick_;
 
 private:
-    void healthTickThread();
+  struct trackDups {
+    trackDups(int totreplicas, int emptyID = -1)
+      : recv_replicas_(totreplicas * totreplicas, 0)
+      , recv_views_(totreplicas, emptyID)
+      , empty_id (emptyID)
+      {}
+    std::vector<int8_t> recv_replicas_;
+    std::vector<int> recv_views_;
+    int empty_id;
+  };
+  trackDups trackDups_SVCs_;
+  trackDups trackDups_DVCs_;
+  trackDups trackDups_PrepResps_;
+  trackDups trackDups_SVResps_;
+  std::vector<MsgStartViewResponse> svResps_;
 
-    volatile bool continue_healthtick_;
-    std::thread healthTickThread_;
+  std::pair<bool,int>
+  checkDuplicate(trackDups&, int from, int view);
+  void clearDupsEntry(trackDups& td, int idx = -1);
+
+private:
+  void healthTickThread();
+
+  volatile bool continue_healthtick_;
+  std::thread healthTickThread_;
 };
 
 }
--- src/msgs.hpp
+++ src/msgs.hpp
@@ -10,23 +10,23 @@
 namespace vsrepl {
 
 struct MsgClientOp {
-    int clientid;
-    std::string opstr;
-    uint64_t cliopid; // prevents opstr to re-execute together with clientid
+  int clientid;
+  std::string opstr;
+  uint64_t cliopid; // prevents opstr to re-execute together with clientid
 
-    std::string toString() const {
-        return std::to_string(clientid) + "/" + std::to_string(cliopid) + "/" + opstr;
-    }
-    bool operator==(const vsrepl::MsgClientOp& o) const noexcept {
-        return o.clientid == clientid && o.opstr == opstr && o.cliopid == cliopid;
-    }
-    std::size_t hash() const noexcept {
-        auto h1 = std::hash<int>{}(clientid);
-        auto h2 =  std::hash<std::string>{}(opstr);
-        auto h3 =  std::hash<uint64_t>{}(cliopid);
-        h1 ^= (h2 << 1);
-        return h1 ^ (h3 << 1);
-    }
+  std::string toString() const {
+    return std::to_string(clientid) + "/" + std::to_string(cliopid) + "/" + opstr;
+  }
+  bool operator==(const vsrepl::MsgClientOp& o) const noexcept {
+    return o.clientid == clientid && o.opstr == opstr && o.cliopid == cliopid;
+  }
+  std::size_t hash() const noexcept {
+    auto h1 = std::hash<int>{}(clientid);
+    auto h2 =  std::hash<std::string>{}(opstr);
+    auto h3 =  std::hash<uint64_t>{}(cliopid);
+    h1 ^= (h2 << 1);
+    return h1 ^ (h3 << 1);
+  }
 };
 
 struct MsgLeaderRedirect {
@@ -35,66 +35,66 @@ struct MsgLeaderRedirect {
 };
 
 struct MsgPrepare {
-    int view;
-    int op;
-    int commit;
-    std::size_t loghash;
-    MsgClientOp cliop;
+  int view;
+  int op;
+  int commit;
+  std::size_t loghash;
+  MsgClientOp cliop;
 };
 
 struct MsgStartViewChange {
-    int view;
+  int view;
 };
 
 struct MsgDoViewChange {
-    int view;
+  int view;
 };
 
 // Leader's command to all possible Followers
 struct MsgStartView {
-    int view;
-    int last_commit;
+  int view;
+  int last_commit;
 };
 
 // Followers' response to the Leader candidate
 struct MsgStartViewResponse {
-    int view;
-    std::string err;
-    int last_commit;
-    std::vector<std::pair<int, MsgClientOp>> missing_entries;
+  int view;
+  std::string err;
+  int last_commit;
+  std::vector<std::pair<int, MsgClientOp>> missing_entries;
 };
 
 struct MsgPrepareResponse {
-    std::string err;
-    int op;
+  std::string err;
+  int op;
 };
 
 struct MsgGetMissingLogs {
-    int view;
-    int my_last_commit;
+  int view;
+  int my_last_commit;
 };
 
 struct MsgMissingLogsResponse {
-    int view;
-    std::string err;
-    std::pair<int, MsgClientOp> op_log;
-    std::vector<std::pair<int, MsgClientOp>> comitted_logs;
-    unsigned hashoflast;
+  int view;
+  std::string err;
+  std::pair<int, MsgClientOp> op_log;
+  std::vector<std::pair<int, MsgClientOp>> comitted_logs;
+  unsigned hashoflast;
 };
 
 struct MsgPersistedCliOp {
-    int view;
-    uint64_t cliopid;
+  int view;
+  uint64_t cliopid;
 };
 
 struct MsgOpPersistedQuery {
-    MsgPersistedCliOp perscliop;
+  MsgPersistedCliOp perscliop;
 };
 
 struct MsgOpPersistedResponse {
-    std::string err;
-    MsgPersistedCliOp perscliop;
-    bool exists;
+  std::string err;
+  MsgPersistedCliOp perscliop;
+  bool exists;
 };
 }
 

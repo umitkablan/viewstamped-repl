da603b4824c859236a59c9383d9a0df34db6443c
Umit Kablan <umitkablan@gmail.com>
2022-02-01 19:42:14 +0300

    rafact: crate hasher_test.cpp

--- src/core_test.cpp
+++ src/core_test.cpp
@@ -24,26 +24,6 @@ using ::testing::StrictMock;
 using std::this_thread::sleep_for;
 using VSRETestType = ViewstampedReplicationEngine<MockTMsgDispatcher, MockStateMachine>;
 
-TEST(HasherTest, BasicHashAndMerge_Repeating)
-{
-  std::vector<std::pair<int, MsgClientOp>> vv {
-    { 0, { 1, "x=3", 1 } },
-    { 2, { 1, "x=4", 2 } },
-    { 3, { 2, "y=0", 1 } },
-    { 4, { 1, "x=-1", 3 } },
-  };
-  const auto hAll = mergeLogsHashes(vv.begin(), vv.end());
-  const auto h0 = mergeLogsHashes(vv.begin(), vv.begin() + 2);
-  const auto h2 = mergeLogsHashes(vv.begin() + 2, vv.begin() + 3, h0);
-  const auto h3 = mergeLogsHashes(vv.begin() + 3, vv.begin() + 4, h2);
-
-  ASSERT_EQ(hAll, h3);
-  // below values change depending on (libc++) platform - fails during PR validation
-  // ASSERT_EQ(std::size_t(10782676624795537932ull), h0);
-  // ASSERT_EQ(std::size_t(11415403314268682002ull), h2);
-  // ASSERT_EQ(std::size_t(15084498811304813772ull), h3);
-}
-
 TEST(CoreTest, BasicDoViewChange)
 {
   StrictMock<MockTMsgDispatcher> msgdispatcher;
